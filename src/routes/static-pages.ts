d033b51545afac7ad8f4cc70f8260ad2d2acb45c
zmontgo <31627113+zmontgo@users.noreply.github.com>
2022-04-02 15:09:02 -0500

    Add various pages

--- src/routes/static-pages.ts
+++ src/routes/static-pages.ts
@@ -3,9 +3,11 @@ import { PublicError } from "../components/sentry";
 
 export function init() {
   router.get("/", async (ctx) => {
-    ctx.render("index.pug", {
-      title: "Home | Allusian",
-    });
+    const admin = true;
+
+    if (admin) {
+      ctx.render("admin.pug");
+    }
   });
 
   router.get("/humans.txt", async (ctx) => {
