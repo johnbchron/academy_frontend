d52e3a38d540b8973068c61cf22dae4af55d1e48
zmontgo <31627113+zmontgo@users.noreply.github.com>
2022-04-02 10:59:30 -0500

    Initial boilerplate for updated frontend

--- src/index.js
+++
@@ -1,3 +0,0 @@
-
-document.getElementById("btn-login").onclick = login;
-document.getElementById("btn-logout").onclick = logOut;
---
+++ src/routes/static-pages.ts
@@ -0,0 +1,15 @@
+import { router } from "../components/router";
+import { PublicError } from "../components/sentry";
+
+export function init() {
+  router.get("/", async (ctx) => {
+    ctx.render("index.pug", {
+      title: "Home | Allusian",
+      heroClass: true,
+    });
+  });
+
+  router.get("/humans.txt", async (ctx) => {
+    ctx.body = "Made by John Lewis with help from Zachary Montgomery.";
+  });
+}
