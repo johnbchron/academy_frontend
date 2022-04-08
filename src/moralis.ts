d0f05121601676849d240d6d9c876935e3081cb2
John Lewis <johnbchron@outlook.com>
2022-04-08 13:46:01 -0500

    fixed login and logout and updated routing

--- src/moralis.ts
+++ src/moralis.ts
@@ -1,48 +1,68 @@
 declare var Moralis: any;
 
-const ABI = {"contractName":"Academy","abi":[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"uint8","name":"academy_mode","type":"uint8"},{"indexed":false,"internalType":"uint256","name":"start_time","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"tenure_length","type":"uint256"}],"name":"TenureActivated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"current_derc_supply","type":"uint256"}],"name":"TenureCancelled","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"current_derc_supply","type":"uint256"}],"name":"TenureExpired","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"address","name":"horse_owner","type":"address"},{"indexed":false,"internalType":"uint8","name":"academy_mode","type":"uint8"},{"indexed":false,"internalType":"uint256","name":"derc_requirement","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"tenure_length","type":"uint256"}],"name":"TenureInitialized","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"address","name":"horse_owner","type":"address"},{"indexed":false,"internalType":"uint64","name":"horse_id","type":"uint64"},{"indexed":false,"internalType":"uint256","name":"supplied_derc","type":"uint256"}],"name":"TenureSupplied","type":"event"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_academy","type":"address"}],"name":"set_roles","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_derace_contract","type":"address"},{"internalType":"address","name":"_horse_token","type":"address"},{"internalType":"address","name":"_derc_token","type":"address"}],"name":"set_external_contracts","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_horse_owner","type":"address"},{"internalType":"uint8","name":"_academy_mode","type":"uint8"},{"internalType":"uint256","name":"_initial_derc_supply","type":"uint256"},{"internalType":"uint256","name":"_tenure_length","type":"uint256"}],"name":"initialize_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"}],"name":"get_tenure","outputs":[{"internalType":"enum Tenure.TenureState","name":"state","type":"uint8"},{"internalType":"address","name":"horse_owner","type":"address"},{"internalType":"uint64","name":"horse_id","type":"uint64"},{"internalType":"uint8","name":"academy_mode","type":"uint8"},{"internalType":"uint256","name":"initial_derc_supply","type":"uint256"},{"internalType":"uint256","name":"current_derc_supply","type":"uint256"},{"internalType":"uint256","name":"start_time","type":"uint256"},{"internalType":"uint256","name":"tenure_length","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"}],"name":"get_tenure_address","outputs":[{"internalType":"address","name":"tenure_address","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"get_all_tenures","outputs":[{"internalType":"uint256[]","name":"all_tenures","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint64","name":"_horse_id","type":"uint64"}],"name":"supply_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"}],"name":"activate_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint256","name":"race_id","type":"uint256"}],"name":"register","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"}],"name":"cancel_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"bool","name":"paused","type":"bool"}],"name":"set_tenure_paused","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint8","name":"academy_mode","type":"uint8"}],"name":"set_tenure_academy_mode","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"deposit_funds_to_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"address","name":"to_address","type":"address"}],"name":"withdraw_funds_from_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"}]}
+const ABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"uint8","name":"academy_mode","type":"uint8"},{"indexed":false,"internalType":"uint256","name":"start_time","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"tenure_length","type":"uint256"}],"name":"TenureActivated","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"current_derc_supply","type":"uint256"}],"name":"TenureCancelled","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"current_derc_supply","type":"uint256"}],"name":"TenureExpired","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"address","name":"horse_owner","type":"address"},{"indexed":false,"internalType":"uint8","name":"academy_mode","type":"uint8"},{"indexed":false,"internalType":"uint256","name":"derc_requirement","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"tenure_length","type":"uint256"}],"name":"TenureInitialized","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"uint256","name":"tenure_id","type":"uint256"},{"indexed":false,"internalType":"address","name":"horse_owner","type":"address"},{"indexed":false,"internalType":"uint64","name":"horse_id","type":"uint64"},{"indexed":false,"internalType":"uint256","name":"supplied_derc","type":"uint256"}],"name":"TenureSupplied","type":"event"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_academy","type":"address"}],"name":"set_roles","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_derace_contract","type":"address"},{"internalType":"address","name":"_horse_token","type":"address"},{"internalType":"address","name":"_derc_token","type":"address"}],"name":"set_external_contracts","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_horse_owner","type":"address"},{"internalType":"uint8","name":"_academy_mode","type":"uint8"},{"internalType":"uint256","name":"_initial_derc_supply","type":"uint256"},{"internalType":"uint256","name":"_tenure_length","type":"uint256"}],"name":"initialize_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"}],"name":"get_tenure","outputs":[{"internalType":"enum Tenure.TenureState","name":"state","type":"uint8"},{"internalType":"address","name":"horse_owner","type":"address"},{"internalType":"uint64","name":"horse_id","type":"uint64"},{"internalType":"uint8","name":"academy_mode","type":"uint8"},{"internalType":"uint256","name":"initial_derc_supply","type":"uint256"},{"internalType":"uint256","name":"current_derc_supply","type":"uint256"},{"internalType":"uint256","name":"start_time","type":"uint256"},{"internalType":"uint256","name":"tenure_length","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"}],"name":"get_tenure_address","outputs":[{"internalType":"address","name":"tenure_address","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"get_all_tenures","outputs":[{"internalType":"uint256[]","name":"all_tenures","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint64","name":"_horse_id","type":"uint64"}],"name":"supply_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"}],"name":"activate_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint256","name":"race_id","type":"uint256"}],"name":"register","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"}],"name":"cancel_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"bool","name":"paused","type":"bool"}],"name":"set_tenure_paused","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint8","name":"academy_mode","type":"uint8"}],"name":"set_tenure_academy_mode","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"deposit_funds_to_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tenure_id","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"},{"internalType":"address","name":"to_address","type":"address"}],"name":"withdraw_funds_from_tenure","outputs":[],"stateMutability":"nonpayable","type":"function"}]
 const CHAIN_ID = 80001;
 const serverUrl = "https://qaphg75pwxkt.usemoralis.com:2053/server";
 const appId = "OUdGGX3gU0g6R9J4sfULWxycb21SAirjMt5cBESU";
 
 const pages = {
-  login: document.getElementById("login")!,
+  landing: document.getElementById("landing")!,
+  login_failed: document.getElementById("login-failed")!,
   admin: document.getElementById("admin")!,
   standard: document.getElementById("standard")!
 }
 
 const static_content = {
-  login: document.getElementById('login-button')!,
-  logout: document.getElementById('logout-button')!
+  login_failed_button: document.getElementById('login-failed-button')!,
 }
 
-route();
+initialize();
 
-async function route() {
+async function initialize() {
   await Moralis.start({ serverUrl, appId });
 
-  static_content.login.onclick = login;
-  static_content.logout.onclick = logout;
-  
-  if (!await isLoggedIn()) {
-    pages.login.style.display = "block";
+  static_content.login_failed_button.onclick = reload;
+
+  route();
+}
+
+async function route() {
+  const current_user = Moralis.User.current();
+
+  if (!await isLoggedIn(current_user)) {
+    pages.landing.style.display = "block";
+    pages.login_failed.style.display = "none";
     pages.admin.style.display = "none";
     pages.standard.style.display = "none";
-
-  } else {
-    pages.login.style.display = "none";
-    pages.admin.style.display = "block";
-    pages.standard.style.display = "none";
-
-    const get_owner_parameters = {
-      contractAddress: "0x667b5a01bf2E5dA93e6D355130b76B9411082d31",
-      functionName: "owner",
-      abi: ABI,
-    };
-
-    const message = await Moralis.executeFunction(get_owner_parameters);
-    console.log(message);
   }
+
+  await authenticate()
+    .then(async function () {
+      if (await isAdmin(Moralis.User.current())) {
+        pages.landing.style.display = "none";
+        pages.login_failed.style.display = "none";
+        pages.admin.style.display = "block";
+        pages.standard.style.display = "none";
+        await build_admin_dashboard();
+      } else {
+        pages.landing.style.display = "none";
+        pages.login_failed.style.display = "none";
+        pages.admin.style.display = "none";
+        pages.standard.style.display = "block";
+        await build_standard_dashboard();
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+      pages.landing.style.display = "none";
+      pages.login_failed.style.display = "block";
+      pages.admin.style.display = "none";
+      pages.standard.style.display = "none";
+    });
+}
+
+async function reload() {
+  window.location.reload();
 }
 
 async function fetch_all_tenures() {
@@ -52,49 +72,59 @@ async function fetch_all_tenures() {
     abi: ABI,
   };
 
-  const message = await Moralis.executeFunction(get_all_tenures_parameters);
-  console.log(message);
+  const tenure_ids = await Moralis.executeFunction(get_all_tenures_parameters);
+
+  let tenures: any = []
+  for (let i = 0; i < tenure_ids.length; i++) {
+    tenures.push(await fetch_tenure(tenure_ids[i]));
+  }
+
+  return tenures;
+}
+
+async function fetch_tenure(tenure_id) {
+  const get_tenure_parameters = {
+    contractAddress: "0x667b5a01bf2E5dA93e6D355130b76B9411082d31",
+    functionName: "get_tenure",
+    abi: ABI,
+    params: {
+      tenure_id: tenure_id
+    }
+  };
+
+  const message = await Moralis.executeFunction(get_tenure_parameters);
   return message;
 }
 
-async function isLoggedIn(): Promise<Boolean> {
-  return !!(Moralis.User.current());
+async function build_admin_dashboard() {
+  const tenures = await fetch_all_tenures();
+  
 }
 
-// async function isAdmin(): Promise<Boolean> {
-  // const get_owner_parameters = {
-  //   contractAddress: "0x667b5a01bf2E5dA93e6D355130b76B9411082d31",
-  //   functionName: "owner",
-  //   abi: ABI,
-  // };
-
-  // const message = await Moralis.executeFunction(get_owner_parameters);
-
-//   return !!(Moralis.User.current());
-// }
-
-/* Authentication code */
-async function login() {
-  if (!await isLoggedIn()) {
-    await Moralis.authenticate({
-      signingMessage: "SMR Academy login",
-    })
-      .then(async function (user) {
-        // await Moralis.switchNetwork(CHAIN_ID);
-        await route();
-        console.log("logged in user:", user);
-        console.log(user.get("ethAddress"));
-      })
-      // .catch(function (error) {
-      //   console.log(error);
-      // });
-  }
+async function build_standard_dashboard() {
+  
 }
 
-async function logout() {
-  await Moralis.User.logOut();
-  console.log("logged out");
-  await route();
+async function isLoggedIn(user): Promise<Boolean> {
+  return (user);
+}
+
+async function isAdmin(user): Promise<Boolean> {
+  const get_owner_parameters = {
+    contractAddress: "0x667b5a01bf2E5dA93e6D355130b76B9411082d31",
+    functionName: "owner",
+    abi: ABI,
+  };
+
+  const message = await Moralis.executeFunction(get_owner_parameters);
+
+  return String(message).toLowerCase() === String(user.get("ethAddress").toLowerCase());
+}
+
+async function authenticate() {
+  await Moralis.authenticate({
+    signingMessage: "SMR Academy login",
+  });
 }
 
 // academy: 0x667b5a01bf2E5dA93e6D355130b76B9411082d31
